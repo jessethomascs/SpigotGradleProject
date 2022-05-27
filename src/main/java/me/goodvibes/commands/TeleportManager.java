b4caeb142113e7ce30f068393dc128af7f9ff861
jessethomascs <56457691+jessethomascs@users.noreply.github.com>
2022-05-27 11:01:32 -0400

    Java syntax is wack

--- src/main/java/me/goodvibes/commands/TeleportManager.java
+++ src/main/java/me/goodvibes/commands/TeleportManager.java
@@ -11,13 +11,13 @@ public class TeleportManager implements CommandExecutor {
     @Override
     public boolean onCommand(@NonNull CommandSender sender, @NonNull Command command, @NonNull String label, @NonNull String[] args) {
         if (sender instanceof Player player) {
-            if (command.getName() == "TeleportManager") {
+            if (command.getName().equals("TeleportManager")) {
                 player.sendMessage("TeleportManager: Version 1.0");
                 player.sendMessage("----------------------------");
                 player.sendMessage("/TeleportManager: Gives overview");
                 player.sendMessage("/tpa: Teleport request a player");
                 return true;
-            } else if (command.getName() == "tpa") {
+            } else if (command.getName().equals("tpa")) {
                 return true;
             } else {
                 player.sendMessage("Your command entered was: " + command.getName());
