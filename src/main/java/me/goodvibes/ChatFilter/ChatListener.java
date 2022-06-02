6d947dc67e9856735cda30d833199796689d691e
Jesse Thomas <56457691+jessethomascs@users.noreply.github.com>
2022-06-02 12:59:38 -0400

    [+] Basic test functionality in ChatListener.java [+] Basic kick outline in cKick.java [+] Updated plugin.yml

--- src/main/java/me/goodvibes/ChatFilter/ChatListener.java
+++ src/main/java/me/goodvibes/ChatFilter/ChatListener.java
@@ -11,13 +11,34 @@ package me.goodvibes.ChatFilter;
 import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.checkerframework.checker.nullness.qual.NonNull;
+
+import java.util.Locale;
 
 public class ChatListener implements Listener {
 
     @EventHandler
-    public void onPlayerJoin(PlayerJoinEvent event) {
-        //event.setJoinMessage(); Implement Config SQL Option
-        Bukkit.broadcastMessage("Welcome " + event.getPlayer().getDisplayName() + " to GoodVibesOnly!");
+    public void AsyncPlayerChatEvent(AsyncPlayerChatEvent chat) {
+        if (chat.getMessage().equals("who's joe".toLowerCase(Locale.ROOT))) { // todo: this is a testing function, remove after use
+            chat.getPlayer().sendMessage("original: " + chat.getMessage());
+            chat.getPlayer().sendMessage("Your format: " + chat.getFormat());
+            chat.getPlayer().sendMessage("Event name: " + chat.getEventName());
+            chat.setMessage("JOE MAMA");
+        }
+
+        /// FUNCTION STRUCTURE ///
+        /*
+            Get player chat here, check for profanity/slurs/targeted harassment
+        */
+
+        /*public void army nation() lol
+            Action for message here, ban for slur, warn, kick, etc.
+        */
+
+        /*public void army nation() lol
+            Call 'UploadChat.java' and send to Discord (this being last is important)
+        */
     }
 }
--- src/main/java/me/goodvibes/commands/BanManager/cKick.java
+++ src/main/java/me/goodvibes/commands/BanManager/cKick.java
@@ -3,11 +3,12 @@ Class: cKick.java
 permission: GoodVibesOnly.BanManager.cKick
 Author: Jesse Thomas, jesse.thomas@snhu.edu
 Hierarchy: BanManager subclass
-Purpose: Kicks a player from the server, logging incident report for kick to database
+Purpose: Kicks a player from the server
 */
 
 package me.goodvibes.commands.BanManager;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -18,8 +19,17 @@ public class cKick implements CommandExecutor {
 
     @Override
     public boolean onCommand(@NonNull CommandSender sender, @NonNull Command command, @NonNull String label, @NonNull String[] args) {
-        if (sender instanceof Player player) {
+        if (args.length < 1 || args[0].length() == 0) { // No target given
+            sender.sendMessage("Invalid syntax. Usage: /kick <target> <reason>");
+            return false;
+        }
 
+        Player target = Bukkit.getPlayerExact(args[0]);
+        String reason = (args[1].length() < 1) ? ("Kicked by operator") : args[1];
+        if (target != null) {
+            target.kickPlayer(reason);
+        } else {
+            sender.sendMessage("Target is not online.");
         }
         return true;
     }
